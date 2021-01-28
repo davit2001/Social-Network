2e45daa5ec2f4770b1b227152d21ac60373edf83
gevorgabgaryan <gevorg.gak@gmail.com>
2021-01-28 19:09:10 +0400

    p

--- app.js
+++ app.js
@@ -40,9 +40,11 @@ app.set('view options', {
 
 
 // server
-server.listen(3000, () => {
-  console.log("server listening port-> 3000");
+let  port = process.env.PORT || '3000';
+server.listen(port, () => {
+  console.log(`server listening port->${port}`);
 });
+
 
 // Restful API`s
 app.use('/', indexRouter)
